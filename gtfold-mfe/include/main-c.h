90ea5ed049588ad424fc086ea42d5754e0ef7ae2
Andrew Ash <ash211@gmail.com>
2011-01-27 11:53:14 -0500

    Remove more of DYNALLOC

--- gtfold-mfe/include/main-c.h
+++ gtfold-mfe/include/main-c.h
@@ -20,9 +20,6 @@
 #ifndef _MAIN_C_H
 #define _MAIN_C_H
 
-#define DYNALLOC
-
-#ifdef DYNALLOC
 extern int LENGTH;
 extern unsigned char *RNA1; /* [LENGTH] */
 extern unsigned char *RNA; /* [LENGTH] */
@@ -34,20 +31,6 @@ extern int **VM; /* [LENGTH][LENGTH] */
 extern int **WM; /* [LENGTH][LENGTH] */
 extern int *indx; /* [LENGTH] */
 extern int *constraints;
-#else
-
-#define LENGTH 8500
-
-extern unsigned char RNA[LENGTH];
-extern unsigned char RNA1[LENGTH];
-extern int structure[LENGTH];
-extern int V[(LENGTH-1)*(LENGTH)/2 + 1];
-extern int VBI[LENGTH][LENGTH];
-extern int VM[LENGTH][LENGTH];
-extern int WM[LENGTH][LENGTH];
-extern int W[LENGTH];
-extern int indx[LENGTH];
-#endif
 
 #define fourBaseIndex(a, b, c, d) (((a) << 6) + ((b) << 4) + ((c) << 2) + (d))
 
