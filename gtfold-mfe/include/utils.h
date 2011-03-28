4012c3abe550a57908d99bf81b641787fe6e9251
Prashant Gaurav <pgaurav3@gatech.edu>
2011-03-27 21:51:18 -0400

    added macro find min of 4 no.s

--- gtfold-mfe/include/utils.h
+++ gtfold-mfe/include/utils.h
@@ -6,6 +6,7 @@
 
 #define MIN(X,Y) ((X) < (Y) ? (X) : (Y))
 #define MAX(X,Y) ((X) > (Y) ? (X) : (Y))
+#define MIN4(W,X,Y,Z) MIN(MIN(W,X),MIN(Y,Z))
 
 char baseToDigit(const char* base) ;
 unsigned char encode(char base);
