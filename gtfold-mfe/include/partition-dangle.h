69de0414f0d551bd4e2b22616057e4239eb25068
Andrew Ash <ash211@gmail.com>
2011-06-16 23:29:19 -0700

    Accessing C declarations from C++ requires the "C" extern

--- gtfold-mfe/include/partition-dangle.h
+++ gtfold-mfe/include/partition-dangle.h
@@ -1,6 +1,9 @@
 #ifndef _PARTITION_DANGLE_H
 #define _PARTITION_DANGLE_H
 
+#ifdef __cplusplus
+extern "C" {
+#endif
 
 typedef struct partition_d{
 	double ** u;
@@ -16,5 +19,9 @@ typedef struct partition_d{
 double cond_dangle(int j, int h, int l);
 dangle_struct malloc_partition_arrays_d(int len);
 void fill_partition_arrays_d(dangle_struct part_struct);
+
+#ifdef __cplusplus
+}
+#endif
 
 #endif
